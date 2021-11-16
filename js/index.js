eec10299502f5694b48196e8e5326db4818ca3ad
FlareFlo <florianlipp17@gmail.com>
2021-11-16 22:16:34 +0100

    Re-added testing functions
    Took 25 minutes

--- js/index.js
+++ js/index.js
@@ -1,18 +1,16 @@
 async function main() {
 	const rust = await import ("../pkg/index.js").catch(console.error);
 
-		rust.test_bind("test bindgen rust call");
+	rust.test_bind("test bindgen rust call");
 
-		fetch_stuff();
-
-		function fetch_stuff() {
-			fetch("http://localhost:8111/indicators").then(function (myJson) {
-				console.log(myJson);
-			})
-			fetch("http://localhost:8111/state").then(function (myJson) {
-				console.log(myJson);
-			})
-		}
+	function fetch_stuff() {
+		fetch("http://localhost:8111/indicators").then(function (myJson) {
+			console.log(myJson);
+		})
+		fetch("http://localhost:8111/state").then(function (myJson) {
+			console.log(myJson);
+		})
+	}
 
 	function sleep(ms) {
 		return new Promise(resolve => setTimeout(resolve, ms));
