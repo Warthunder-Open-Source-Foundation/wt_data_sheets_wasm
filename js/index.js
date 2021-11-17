2e206f41e2dc9cb35bda239a0cfa6b82244c8b53
FlareFlo <florianlipp17@gmail.com>
2021-11-17 23:20:19 +0100

    Added first instance of live missile calculations
    Took 4 hours 23 minutes

--- js/index.js
+++ js/index.js
@@ -1,5 +1,42 @@
 async function main() {
-	const rust = await import ("../pkg/index.js").catch(console.error);
+
+	let rust;
+
+	let url = window.location.href.split("/").at(-1)
+
+	// Custom section for each page to make sure it runs properly
+	if (url == "") {
+		rust = await import ("../pkg/index.js").catch(console.error);
+	}
+
+	if (url == "live_calc.html") {
+		rust = await import ("../pkg/index.js").catch(console.error);
+		rust.make_option_inputs();
+
+		let missile_select = document.getElementById("missile_select");
+
+		missile_select.addEventListener('change', async (e) => {
+			let target = e.target;
+			while (missile_select.value !== -1) {
+				console.log(missile_select.value);
+				let do_splash = document.getElementById("use_splash").checked;
+				await fetch("http://localhost:8111/state").then(function (response) {
+					return response.json();
+
+				}).then(function (data) {
+					let velocity = data["IAS, km/h"];
+					let alt = data["H, m"];
+					rust.constant_calc(velocity, alt, parseInt(target.value), do_splash);
+
+				}).catch(function (error) {
+					console.log("error: " + error);
+				});
+				await sleep(100);
+			}
+		})
+
+
+	}
 
 
 	rust.console_log("This is rust!");
