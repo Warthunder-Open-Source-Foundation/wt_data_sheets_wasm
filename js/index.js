f54f942f26459693e1060248e0e45b0e959fdb18
FlareFlo <florianlipp17@gmail.com>
2021-11-19 18:27:45 +0100

    Added full compare functions
    Took 2 hours 8 minutes

--- js/index.js
+++ js/index.js
@@ -29,40 +29,130 @@ async function main() {
 
 	if (url == "live_calc.html") {
 		rust = await import ("../pkg/index.js").catch(console.error);
-		rust.generate_targets();
+		rust.make_comparison();
+		sleep(100);
+		document.getElementById("dropdown").addEventListener("submit", set_value_enter);
+		input_manager();
+		while (true) {
+			await fetch("http://localhost:8111/state").then(function (response) {
+				return response.json();
 
-		let missile_select = document.getElementById("missile_select");
+			}).then(function (data) {
+				let target = document.getElementById("ul_input").getAttribute("selected");
+				if (data["valid"] === true && !(target === "")) {
+					let velocity = data["IAS, km/h"];
+					let alt = data["H, m"];
+					rust.initiate_calc(velocity, alt, parseInt(target));
+				}
 
-		missile_select.addEventListener('change', async (e) => {
-			let target = e.target;
-			while (true) {
-				let do_splash = document.getElementById("use_splash").checked;
-				await fetch("http://localhost:8111/state").then(function (response) {
-					return response.json();
-
-				}).then(function (data) {
-					if (data["valid"] === true) {
-						let velocity = data["IAS, km/h"];
-						let alt = data["H, m"];
-						rust.constant_calc(velocity, alt, parseInt(target.value), do_splash);
-					}
-
-				}).catch(function (error) {
-					console.log("error: " + error);
-				});
-				await sleep(50);
-			}
-		})
+			}).catch(function (error) {
+				console.log("error: " + error);
+			});
+			await sleep(16);
+		}
 	}
-	if (url == "compare.html") {
+
+	if (url.includes("compare.html")) {
 		rust = await import ("../pkg/index.js").catch(console.error);
 		rust.make_comparison();
+		sleep(100);
+		document.getElementById("dropdown").addEventListener("submit", set_value_enter);
+		input_manager();
+
 	}
 
-	rust.console_log("This is rust!");
 
 	function sleep(ms) {
 		return new Promise(resolve => setTimeout(resolve, ms));
+	}
+
+	function set_value_enter() {
+		document.getElementById("ul_input").setAttribute("class", "value-list");
+		let ul = document.getElementById("ul_input");
+		let elems = ul.getElementsByClassName("select_0");
+		for (var i = 0; i < elems.length; ++i) {
+			if (!elems[i].className.includes("closed")) {
+				document.getElementById("ul_input").setAttribute("selected", i.toString());
+				document.getElementById("ul_input").setAttribute("target_name", elems[i].innerHTML);
+				document.getElementById("input_select").value = elems[i].innerHTML;
+				break;
+			}
+		}
+	}
+
+	function input_manager() {
+		const inputField = document.querySelector(".chosen-value");
+		const dropdown = document.querySelector(".value-list");
+		const dropdownArray = [...document.querySelectorAll("li")];
+		dropdown.classList.add("open");
+		let valueArray = [];
+		dropdownArray.forEach((item) => {
+			valueArray.push(item.textContent);
+		});
+
+		inputField.addEventListener("input", () => {
+			dropdown.classList.add("open");
+			let inputValue = inputField.value.toLowerCase();
+			if (inputValue.length > 0) {
+				for (let j = 0; j < valueArray.length; j++) {
+					let input = inputValue.toLowerCase().substring(0, inputValue.length);
+					let refference = valueArray[j]
+						.replace(valueArray[j].split("_")[0], '').replaceAll('_', '')
+						.substring(0, inputValue.length).toLowerCase();
+					if (
+						!(
+							input === refference
+						)
+					) {
+						dropdownArray[j].classList.add("closed");
+					} else {
+						dropdownArray[j].classList.remove("closed");
+					}
+				}
+			} else {
+				for (let i = 0; i < dropdownArray.length; i++) {
+					dropdownArray[i].classList.remove("closed");
+				}
+			}
+		});
+
+		dropdownArray.forEach((item) => {
+			item.addEventListener("click", (env) => {
+				document.getElementById("ul_input").setAttribute("selected", env.target.value);
+				document.getElementById("ul_input").setAttribute("target_name", env.target.innerHTML);
+				inputField.value = item.textContent;
+				dropdownArray.forEach((dropdown) => {
+					dropdown.classList.add("closed");
+				});
+			});
+		});
+
+		inputField.addEventListener("focus", () => {
+			// Resets values
+			document.getElementById("input_select").value = "";
+			document.getElementById("ul_input").setAttribute("target_name", "");
+			document.getElementById("ul_input").setAttribute("selected", "");
+			document.getElementById("range").innerHTML = "-";
+			document.getElementById("splash_at").innerHTML = "-";
+			inputField.placeholder = "Type to filter";
+			dropdown.classList.add("open");
+			dropdownArray.forEach((dropdown) => {
+				dropdown.classList.remove("closed");
+			});
+		});
+
+		inputField.addEventListener("blur", () => {
+			inputField.placeholder = "Select Missile";
+			dropdown.classList.remove("open");
+		});
+
+		document.addEventListener("click", (evt) => {
+			const isDropdown = dropdown.contains(evt.target);
+			const isInput = inputField.contains(evt.target);
+			if (!isDropdown && !isInput) {
+				dropdown.classList.remove("open");
+			}
+		});
 	}
 }
 
--- src/live_calc.rs
+++ src/live_calc.rs
@@ -4,15 +4,20 @@ use wt_ballistics_calc_lib::runner::generate;
 
 use wasm_bindgen::prelude::*;
 
-use crate::{make_option_inputs, MISSILES};
+use crate::{console_log, make_option_inputs, MISSILES};
 use crate::util::get_document;
 
 #[wasm_bindgen]
 pub fn generate_targets() {
-	make_option_inputs("missile_select", "option", None);
+	make_option_inputs("ul_input", "option", None);
 }
 
 #[wasm_bindgen]
+pub fn initiate_calc(velocity: f64, alt: u32, select: usize) {
+	constant_calc(velocity, alt, select, true)
+}
+
+
 pub fn constant_calc(velocity: f64, alt: u32, missile_select: usize, do_splash: bool) {
 	let mut parameters = LaunchParameter::new_from_parameters(false, (velocity / 3.6), 0.0, (velocity / 3.6), alt);
 
@@ -28,7 +33,7 @@ pub fn constant_calc(velocity: f64, alt: u32, missile_select: usize, do_splash: 
 	if do_splash {
 		while !results.splash.splash {
 			results = generate(&MISSILES[missile_select], &parameters, 0.1, false);
-			parameters.distance_to_target -= 100.0;
+			parameters.distance_to_target -= 10.0;
 		}
 		document.get_element_by_id("splash_at").unwrap().set_inner_html(&results.splash.at.round().to_string());
 	} else {
