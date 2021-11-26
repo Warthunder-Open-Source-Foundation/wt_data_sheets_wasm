2bceb8da4feb88a979700ec14674bf2d5dd3ef26
flareflo <florianlipp17@gmail.com>
2021-11-26 08:55:27 +0100

    Change paths to git submodule
    Took 17 minutes

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@ use lazy_static::lazy_static;
 use wasm_bindgen::JsValue;
 use wasm_bindgen::prelude::*;
 use wt_datamine_extractor_lib::missile::missile::Missile;
+use wt_datamine_extractor_lib::thermal::thermals::Thermal;
 
 use crate::util::make_option_inputs;
 
@@ -12,11 +13,18 @@ pub mod comparison;
 
 lazy_static! {
 	static ref MISSILES: Vec<Missile> = {
-		let json = include_str!("../../wt_datamine_extractor/missile_index/all.json");
+		let json = include_str!("../wt_datamine_extractor/missile_index/all.json");
 		let mut missiles: Vec<Missile> = serde_json::from_str(json).unwrap();
 		missiles.sort_by_key(|d| d.name.clone());
 
 		missiles
+	};
+	static ref THERMALS: Vec<Thermal> = {
+		let json = include_str!("../wt_datamine_extractor/thermal_index/all.json");
+		let mut thermals: Vec<Thermal> = serde_json::from_str(json).unwrap();
+		thermals.sort_by_key(|d| d.name.clone());
+
+		thermals
 	};
 }
 
