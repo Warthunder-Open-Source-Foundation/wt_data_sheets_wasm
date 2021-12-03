552f22c3762231fecdeb392e90082a5c0eb756d7
flareflo <florianlipp17@gmail.com>
2021-12-03 11:00:58 +0100

    Fixed repetition for nightly mode
    Took 1 hour 4 minutes

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@ async function main() {
 
 	let url = window.location.href.split("/").at(-1);
 
-	if (window.location.href.includes("nightly")) {
+	if (url.includes("nightly")) {
 		console.info("ENABLING NIGHTLY MODE")
 		document.querySelector("html").style.setProperty("--background-image-red", "linear-gradient(120deg, #8d8d8d, #343434)");
 		document.querySelector("html").style.setProperty("--color-background", "url(metafiles/WIP.png)");
