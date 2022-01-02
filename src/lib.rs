a4f6ec2cea64f9c8271aa820019b8ec097cab8bc
FlareFlo <florianlipp17@gmail.com>
2022-01-03 00:55:23 +0100

    Added testing failure point for CI
    Took 3 minutes

--- src/lib.rs
+++ src/lib.rs
@@ -14,6 +14,8 @@ pub mod comparison;
 pub mod thermal_index;
 pub mod shell_index;
 
+supposed_to_fail
+
 lazy_static! {
 	static ref MISSILES: Vec<Missile> = {
 		let json = include_str!("../wt_datamine_extractor/missile_index/all.json");
