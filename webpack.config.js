b1fec50e1b8f06d7b6c5bb7b0676c48380370cca
flareflo <florianlipp17@gmail.com>
2022-01-06 20:49:12 +0100

    Removed css from git tracking and moved scss to new dir
    Took 3 minutes
    
    Took 1 minute

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,7 @@ module.exports = {
 	},
 	plugins: [
 		new CopyPlugin([
-			path.resolve(__dirname, "static/*"),
+			path.resolve(__dirname, "static/manifest"),
 			path.resolve(__dirname, "static/css"),
 			path.resolve(__dirname, "static/html"),
 		]),
