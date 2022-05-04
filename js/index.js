101fb92f759cc967c80e46f0e0002c9703464613
FlareFlo <mail@flareflo.dev>
2022-05-05 01:39:36 +0200

    Fixed bug with improper updating to aircraft selection

--- js/index.js
+++ js/index.js
@@ -241,51 +241,15 @@ async function main() {
 		rust.show_aircraft_loadout(0);
 
 		document.getElementById("aircraft").addEventListener("input", function  () {
-			let id = document.getElementById("aircraft").selectedOptions[0].getAttribute("index");
-			rust.show_aircraft_loadout(parseInt(id));
+			rust.show_aircraft_loadout(selectedAircraft());
+			addSelectionListeners();
 		});
 
 		let selectable = document.getElementsByClassName("selectable");
-
-		for (let i = 0; i < selectable.length; i++) {
-			selectable[i].addEventListener("click", function (element, target) {
-				let id = element.composedPath()[1].id;
-				if (id === "") {
-					id = element.composedPath()[0].id;
-				};
-				let split = id.split("_");
-				let col = parseInt(split[0]);
-
-				// Disable selection for previously selected item
-				// There are so many choices because indexes for slot 0 are offset by 1
-				let old_idx = selected[col];
-				if (old_idx !== undefined) {
-					let select = `${col}_${old_idx}`;
-					let old = document.getElementById(select);
-					old.classList.remove("selected");
-				}
-
-				let pre_selected = `${col}_0`;
-				let empty = document.getElementById(pre_selected);
-				if (empty !== null) {
-					empty.classList.remove("selected");
-				}
-
-				let gun_empty = document.getElementById(`${col}_1`);
-				if (gun_empty !== null) {
-					gun_empty.classList.remove("selected");
-				}
-				// Finished cleaning up old selection
-
-
-				// Add new selection to list
-				selected[col] = parseInt(split[1]);
-				document.getElementById(id).classList.add("selected");
-			});
-		}
+		addSelectionListeners();
 
 		document.getElementById("apply_choices").addEventListener("click", function () {
-			rust.output_selection(selected);
+			rust.output_selection(selected, selectedAircraft());
 		});
 
 		document.getElementById("reset_choices").addEventListener("click", function () {
@@ -300,7 +264,53 @@ async function main() {
 				const first_item = row.childNodes[1];
 				first_item.classList.add("selected");
 			}
+
+			document.getElementById("cl_result").innerHTML = "";
 		});
+
+		function addSelectionListeners() {
+			for (let i = 0; i < selectable.length; i++) {
+				selectable[i].addEventListener("click", function (element, target) {
+					let id = element.composedPath()[1].id;
+					if (id === "") {
+						id = element.composedPath()[0].id;
+					};
+					let split = id.split("_");
+					let col = parseInt(split[0]);
+
+					// Disable selection for previously selected item
+					// There are so many choices because indexes for slot 0 are offset by 1
+					let old_idx = selected[col];
+					if (old_idx !== undefined) {
+						let select = `${col}_${old_idx}`;
+						let old = document.getElementById(select);
+						old.classList.remove("selected");
+					}
+
+					let pre_selected = `${col}_0`;
+					let empty = document.getElementById(pre_selected);
+					if (empty !== null) {
+						empty.classList.remove("selected");
+					}
+
+					let gun_empty = document.getElementById(`${col}_1`);
+					if (gun_empty !== null) {
+						gun_empty.classList.remove("selected");
+					}
+					// Finished cleaning up old selection
+
+
+					// Add new selection to list
+					selected[col] = parseInt(split[1]);
+					document.getElementById(id).classList.add("selected");
+				});
+			}
+		}
+
+		function selectedAircraft() {
+			let id = document.getElementById("aircraft").selectedOptions[0].getAttribute("index");
+			return parseInt(id);
+		}
 
 	}
 
