090c145a82e619eaa2fa9ea1d2f1edf10e6ba503
FlareFlo <mail@flareflo.dev>
2022-05-27 18:20:37 +0200

    Added basic bombing computing fn

--- js/index.js
+++ js/index.js
@@ -309,6 +309,23 @@ async function main() {
 
 	}
 
+	if (url.includes("bombing_computer.html")) {
+		while (true) {
+			await fetch("http://localhost:8111/map_obj.json").then(function (response) {
+				return response.json();
+
+			}).then(function (data) {
+				let base_count = document.getElementById("base_count");
+				let string_data = JSON.stringify(data);
+				base_count.innerText = (string_data.split("bombing_point").length - 1) / 2;
+
+			}).catch(function (error) {
+				console.log("error: " + error);
+			});
+			await sleep(1000);
+		}
+	}
+
 	// Misc functions --------------------------------------------------------------------------------------------------
 
 	function sleep(ms) {
