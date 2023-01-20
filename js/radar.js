00522947dc8547a217f71b45b8ce1dfefbff5588
flareflo <github@flareflo.dev>
2023-01-20 10:12:25 +0100

    Disable but keep prototype radar display

--- js/radar.js
+++ js/radar.js
@@ -1,12 +1,14 @@
-import {main_js, run_proto} from "../pkg";
+import {main_js} from "../pkg";
 
 async function main() {
-	let c = 0;
-	const timer = 1000 / 30;
-	const interval = setInterval(function() {
-		run_proto(c);
-		c++;
-	}, timer);
+	// let c = 0;
+	// const timer = 1000 / 30;
+	// const interval = setInterval(function() {
+	// 	run_proto(c);
+	// 	c++;
+	// }, timer);
+
+
 }
 main_js()
 main()
