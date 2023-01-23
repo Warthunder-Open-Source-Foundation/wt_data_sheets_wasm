85151b916d3d01de6c7e4e25ebbc8620695eccf4
flareflo <github@flareflo.dev>
2023-01-23 12:46:52 +0100

    Add submodes, very basic

--- src/radar.rs
+++ src/radar.rs
@@ -1,8 +1,14 @@
 use std::sync::atomic::{AtomicBool, AtomicI32, Ordering};
 use std::sync::atomic::Ordering::Relaxed;
-use wasm_bindgen::prelude::*;
+
+use bevy_reflect::{Reflect, Struct};
 use plotters::prelude::*;
 use plotters_canvas::CanvasBackend;
+use wasm_bindgen::prelude::*;
+use wt_sensor::scanning::scan_pattern::SubmodeCategory;
+use wt_sensor::scanning::scan_type::Pattern;
+use wt_sensor::util::Limits;
+
 use crate::RADAR;
 use crate::util::{console_log, get_document, panic_debug};
 
@@ -19,8 +25,6 @@ pub fn render_table() {
 
 	// Set scan ranges
 	let scan_ranges = document.get_element_by_id("scan_ranges").unwrap();
-	console_log("&i.to_string()");
-
 
 	for i in &RADAR.scope_range_sets.common {
 		let elem = document.create_element("p").unwrap();
@@ -35,6 +39,48 @@ pub fn render_table() {
 		elem.set_inner_html(&((i * 0.001).to_string() + " km"));
 		boresight_ranges.append_child(&elem).unwrap();
 	}
+
+	let submode_rows = document.get_element_by_id("submode_rows").unwrap();
+
+	for mode in &RADAR.submode {
+		let row = document.create_element("tr").unwrap();
+		for iter_field in mode.iter_fields() {
+			let elem = document.create_element("td").unwrap();
+			if let Some(val) = iter_field.downcast_ref::<Option<f64>>() {
+				elem.set_inner_html(&if let Some(out) = val {
+					out.to_string()
+				} else {
+					"-".to_owned()
+				});
+			}
+			if let Some(val) = iter_field.downcast_ref::<Option<bool>>() {
+				elem.set_inner_html(&if let Some(out) = val {
+					out.to_string()
+				} else {
+					"-".to_owned()
+				});
+			}
+			if let Some(val) = iter_field.downcast_ref::<Option<u8>>() {
+				elem.set_inner_html(&if let Some(out) = val {
+					out.to_string()
+				} else {
+					"-".to_owned()
+				});
+			}
+			if let Some(val) = iter_field.downcast_ref::<Pattern>() {
+				elem.set_inner_html(&val.to_string());
+				;
+			}
+			if let Some(val) = iter_field.downcast_ref::<SubmodeCategory>() {
+				elem.set_inner_html(&val.to_string());
+			}
+			if let Some(val) = iter_field.downcast_ref::<Limits>() {
+				elem.set_inner_html(&format!("{:?} {:?}", val.azimuth, val.elevation));
+			}
+			row.append_child(&elem);
+		}
+		submode_rows.append_child(&row);
+	}
 }
 
 #[wasm_bindgen]
@@ -47,7 +93,7 @@ pub fn run_proto(step: i32) {
 	let counter = (step * 10) % 1000;
 
 
-	let radar_angle  = (((step as f64 / 25.0).sin() + 1.0) * 500.0) - 25.0;
+	let radar_angle = (((step as f64 / 25.0).sin() + 1.0) * 500.0) - 25.0;
 
 	// console_log(&format!("{} {}", radar_angle, counter));
 
